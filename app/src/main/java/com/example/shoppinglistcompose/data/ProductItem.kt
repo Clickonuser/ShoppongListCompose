05497205ab7def9588960e3e2aa0436a1f9fa045
clickonuser <clickonuser@gmail.com>
2024-09-26 22:07:27 +0300

    Rename class

--- app/src/main/java/com/example/shoppinglistcompose/data/ProductItem.kt
+++ app/src/main/java/com/example/shoppinglistcompose/data/ProductItem.kt
@@ -3,11 +3,11 @@ package com.example.shoppinglistcompose.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "add_table")
-data class AddItem(
+@Entity(tableName = "product_table")
+data class ProductItem(
     @PrimaryKey
     val id: Int? = null,
     val name: String,
     val isChecked: Boolean,
-    val shoppingListId: Int
+    val mainListId: Int
 )
